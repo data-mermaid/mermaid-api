b3e9b906460b356a3943d9a239af0cfe763112dd
Dustin Sampson <gridcell@gmail.com>
2019-07-31 14:01:22 -0700

    formatting datachanges.py

--- src/tools/management/commands/datachanges.py
+++ src/tools/management/commands/datachanges.py
@@ -1,38 +1,46 @@
 from datetime import date
-from django.utils.encoding import force_bytes
 
-from django.core.management.base import BaseCommand, CommandError
-from django.contrib.contenttypes.models import ContentType
 import dateutil
 import pytz
 
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.contenttypes.models import ContentType
+from django.utils.encoding import force_bytes
+
 
 class Command(BaseCommand):
-    SKIP_MODELS = ("growthform", "appversion", "application", "archivedrecord", "uuidtaggeditem")
+    SKIP_MODELS = (
+        "growthform",
+        "appversion",
+        "application",
+        "archivedrecord",
+        "uuidtaggeditem",
+    )
 
     def add_arguments(self, parser):
-        parser.add_argument('date', type=dateutil.parser.isoparse)
+        parser.add_argument("date", type=dateutil.parser.isoparse)
         parser.add_argument(
-            '--details',
+            "--details",
             dest="show_details",
             action="store_true",
-            help='Details of changes',
+            help="Details of changes",
         )
-    
+
     def get_api_model_classes(self):
-        qs = ContentType.objects.filter(app_label='api').exclude(model__in=self.SKIP_MODELS)
+        qs = ContentType.objects.filter(app_label="api").exclude(
+            model__in=self.SKIP_MODELS
+        )
         return [c.model_class() for c in qs if c.model_class() is not None]
-    
+
     def get_record_details(self, record):
         return dict(
             id=record.pk,
             updated_on=record.updated_on,
             updated_by=record.updated_by or "",
         )
-    
+
     def get_details(self, queryset):
         return [self.get_record_details(rec) for rec in queryset]
-
 
     def handle(self, *args, **kwargs):
         start_date = kwargs.get("date").replace(tzinfo=pytz.utc)
@@ -47,7 +55,7 @@ class Command(BaseCommand):
             details = self.get_details(qs)
             if details:
                 changes[model_name] = details
-        
+
         if show_details is True:
             for c in sorted(changes):
                 for m in changes[c]:
