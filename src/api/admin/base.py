80a00db089a8b24df06d53ba714c6972130e0aed
Kim Fisher <kfisher@wcs.org>
2019-08-20 11:59:11 -0400

    Admin tweaks

--- src/api/admin/base.py
+++ src/api/admin/base.py
@@ -38,7 +38,7 @@ def export_model_as_csv(modeladmin, request, queryset, field_list):
             field_obj, attr, value = admin.utils.lookup_field(field, obj, modeladmin)
             if field_obj is not None and hasattr(field_obj, 'choices'):
                 value = lookup_field_from_choices(field_obj, value)
-            csv_line_values.append(str(value).encode('utf-8').strip())
+            csv_line_values.append(str(value).strip())
 
         writer.writerow(csv_line_values)
 
