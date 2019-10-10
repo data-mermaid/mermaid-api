8bfc32e6dda2560a7eed07bd99f94931110a6ef4
Dustin Sampson <gridcell@gmail.com>
2019-10-10 07:56:07 -0700

    allow all MockRequest properties to be optionally passed in

--- src/api/mocks.py
+++ src/api/mocks.py
@@ -1,8 +1,8 @@
 class MockRequest:
-    def __init__(self, user=None, token=None):
+    def __init__(self, user=None, token=None, query_params=None, GET=None):
         self.user = user
-        self.GET = {}
-        self.query_params = {}
+        self.GET = GET or dict()
+        self.query_params = query_params or dict()
         if token:
             self.META = {"HTTP_AUTHORIZATION": "Bearer {}".format(token)}
         else:
