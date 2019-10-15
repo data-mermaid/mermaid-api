c47ef2409a8bdafbf5f7971a0eaf112570016a0e
Dustin Sampson <gridcell@gmail.com>
2019-10-15 09:05:25 -0700

    add management name/id to sample unit endpoint

--- src/api/resources/sample_units/sample_unit_methods.py
+++ src/api/resources/sample_units/sample_unit_methods.py
@@ -31,6 +31,7 @@ class SearchNonFieldFilter(django_filters.Filter):
     SEARCH_FIELDS = [
         "protocol_name",
         "site_name",
+        "management_name",
         "benthiclit__observers__profile__first_name",
         "benthiclit__observers__profile__last_name",
         "benthicpit__observers__profile__first_name",
@@ -67,6 +68,8 @@ class SampleUnitMethodSerializer(BaseAPISerializer):
     protocol = serializers.SerializerMethodField()
     site_name = serializers.ReadOnlyField()
     site = serializers.SerializerMethodField()
+    management_name = serializers.ReadOnlyField()
+    management = serializers.SerializerMethodField()
     depth = serializers.ReadOnlyField()
     sample_date = serializers.SerializerMethodField()
     sample_unit_number = serializers.SerializerMethodField()
@@ -85,6 +88,10 @@ class SampleUnitMethodSerializer(BaseAPISerializer):
     def get_site(self, o):
         sample_unit = o.sample_unit
         return sample_unit.sample_event.site.id
+    
+    def get_management(self, o):
+        sample_unit = o.sample_unit
+        return sample_unit.sample_event.management.id
 
     def get_sample_date(self, o):
         sample_unit = o.sample_unit
@@ -188,6 +195,29 @@ class SampleUnitMethodView(BaseProjectApiViewSet):
             ),
         )
 
+        management_name_condition = Case(
+            When(
+                benthiclit__transect__sample_event__management__name__isnull=False,
+                then="benthiclit__transect__sample_event__management__name",
+            ),
+            When(
+                benthicpit__transect__sample_event__management__name__isnull=False,
+                then="benthicpit__transect__sample_event__management__name",
+            ),
+            When(
+                habitatcomplexity__transect__sample_event__management__name__isnull=False,
+                then="habitatcomplexity__transect__sample_event__management__name",
+            ),
+            When(
+                bleachingquadratcollection__quadrat__sample_event__management__name__isnull=False,
+                then="bleachingquadratcollection__quadrat__sample_event__management__name",
+            ),
+            When(
+                beltfish__transect__sample_event__management__name__isnull=False,
+                then="beltfish__transect__sample_event__management__name",
+            ),
+        )
+
         sample_unit_number_condition = Case(
             When(
                 benthiclit__transect__number__isnull=False,
@@ -256,6 +286,7 @@ class SampleUnitMethodView(BaseProjectApiViewSet):
         qs = qs.annotate(
             protocol_name=protocol_condition,
             site_name=site_name_condition,
+            management_name=management_name_condition,
             sample_unit_number=sample_unit_number_condition,
             depth=depth_condition,
             sample_date=sample_date_condition,
