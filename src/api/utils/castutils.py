c3cb07040bc56e57684af433b6e50b3f25c15072
Dustin Sampson <gridcell@gmail.com>
2020-02-11 13:52:05 -0800

    catch "" and None and return None decimal cast

--- src/api/utils/castutils.py
+++ src/api/utils/castutils.py
@@ -2,6 +2,9 @@ from decimal import ROUND_HALF_DOWN, Decimal, localcontext
 
 
 def to_decimal(val, max_digits, precision):
+    if str(val) == "" or val is None:
+        return None
+
     with localcontext() as ctx:
         ctx.prec = max_digits
         places = Decimal(10) ** (precision * -1)
