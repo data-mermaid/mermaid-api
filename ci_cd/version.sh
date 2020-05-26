444fbca5b4ac5119b5489f5d0b5865c04f6ecbfe
Dustin Sampson <gridcell@gmail.com>
2020-05-26 09:03:58 -0700

    using --contains instead of --exact-match

--- ci_cd/version.sh
+++ ci_cd/version.sh
@@ -2,7 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 COMMIT=`git log --format="%H" -n 1`
-VERSION=`git describe --exact-match "$COMMIT" || git rev-parse --abbrev-ref HEAD`
+echo "Commit id: $COMMIT"
+VERSION=`git describe --contains "$COMMIT" || git rev-parse --abbrev-ref HEAD`
 echo "VERSION: $VERSION"
 rm -f $DIR/VERSION.txt
 echo $VERSION >> $DIR/VERSION.txt
