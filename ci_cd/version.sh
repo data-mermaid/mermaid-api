9939a6c0f3a34fa3a4e619ae98b15ed381d31bd8
Dustin Sampson <gridcell@gmail.com>
2020-05-26 10:02:48 -0700

    use describe --tags to fetch api version

--- ci_cd/version.sh
+++ ci_cd/version.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-COMMIT=`git log --format="%H" -n 1`
-echo "Commit id: $COMMIT"
-VERSION=`git describe --contains "$COMMIT" || git rev-parse --abbrev-ref HEAD`
+VERSION=`git describe --tags --abbrev=0`
 echo "VERSION: $VERSION"
 rm -f $DIR/VERSION.txt
 echo $VERSION >> $DIR/VERSION.txt
