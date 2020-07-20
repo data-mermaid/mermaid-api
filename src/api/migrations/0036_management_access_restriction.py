58a844e9ad105750bdf19abf3d20d46332ffb2c3
Dustin Sampson <gridcell@gmail.com>
2020-07-20 15:32:49 -0700

    dev: migration dependency

--- src/api/migrations/0036_management_access_restriction.py
+++ src/api/migrations/0036_management_access_restriction.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0035_merge_20200715_1634'),
+        ('api', '0033_auto_20200717_1528'),
     ]
 
     operations = [
