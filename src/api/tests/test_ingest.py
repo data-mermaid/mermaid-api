cb0ebcbbd7cf98dc8e39b0aae601df6a89eaae54
Dustin Sampson <gridcell@gmail.com>
2020-09-09 08:00:10 -0700

    M1357-1: additional checks for benthic pit and bleaching ingest tests

--- src/api/tests/test_ingest.py
+++ src/api/tests/test_ingest.py
@@ -1,7 +1,5 @@
 import os
 
-from datetime import datetime
-
 import pytest
 from api.ingest import utils
 from api.models import (
@@ -81,7 +79,6 @@ def test_fishbelt_ingest(
     assert observations[2].get("size") == 20
     assert observations[2].get("count") == 1
     assert observations[2].get("fish_attribute") == str(fish_species3.id)
-    
 
 
 def test_benthicpit_ingest(
@@ -92,10 +89,11 @@ def test_benthicpit_ingest(
     base_project,
     all_test_benthic_attributes,
     tide1,
-    tide2,
     relative_depth1,
-    benthic_attribute_2a,
-    growth_form4,
+    benthic_attribute_2a1,
+    growth_form1,
+    site1,
+    management1,
 ):
     new_records, output = utils.ingest(
         protocol=BENTHICPIT_PROTOCOL,
@@ -113,15 +111,25 @@ def test_benthicpit_ingest(
 
     assert new_records is not None and len(new_records) == 2
 
-    new_record = new_records[0]
+    new_record = new_records[1]
+    sample_event = new_record.data.get("sample_event")
     benthic_transect = new_record.data.get("benthic_transect")
     observations = new_record.data["obs_benthic_pits"]
 
-    assert benthic_transect.get("tide") == str(tide2.id)
+    assert new_record.project == project1
+    assert new_record.profile == profile1
 
-    assert len(observations) == 24
-    assert observations[2].get("attribute") == str(benthic_attribute_2a.id)
-    assert observations[2].get("growth_form") == str(growth_form4.id)
+    assert benthic_transect.get("depth") == 8.0
+    assert benthic_transect.get("tide") == str(tide1.id)
+    assert benthic_transect.get("relative_depth") == str(relative_depth1.id)
+
+    assert sample_event.get("site") == str(site1.id)
+    assert sample_event.get("management") == str(management1.id)
+    assert str(sample_event.get("sample_date")) == "2011-03-31"
+
+    assert len(observations) == 3
+    assert observations[0].get("attribute") == str(benthic_attribute_2a1.id)
+    assert observations[0].get("growth_form") == str(growth_form1.id)
 
 
 def test_bleaching_ingest(
@@ -138,6 +146,8 @@ def test_bleaching_ingest(
     benthic_attribute_2,
     benthic_attribute_2a1,
     growth_form4,
+    site1,
+    management1,
 ):
     new_records, output = utils.ingest(
         protocol=BLEACHINGQC_PROTOCOL,
@@ -156,11 +166,25 @@ def test_bleaching_ingest(
     assert new_records is not None and len(new_records) == 1
 
     new_record = new_records[0]
+    sample_event = new_record.data.get("sample_event")
     quadrat_collection = new_record.data.get("quadrat_collection")
+    obs_colonies_bleached = new_record.data["obs_colonies_bleached"]
+
+    print(new_record.data)
+
+    assert new_record.project == project1
+    assert new_record.profile == profile1
 
     assert quadrat_collection.get("tide") == str(tide1.id)
+    assert quadrat_collection.get("depth") == 1.0
+    assert quadrat_collection.get("quadrat_size") == 1
+    assert quadrat_collection.get("visibility") == str(visibility1.id)
+    assert quadrat_collection.get("current") == str(current3.id)
+    assert quadrat_collection.get("relative_depth") == str(relative_depth1.id)
 
-    obs_colonies_bleached = new_record.data["obs_colonies_bleached"]
+    assert sample_event.get("site") == str(site1.id)
+    assert sample_event.get("management") == str(management1.id)
+    assert str(sample_event.get("sample_date")) == "2020-05-20"
 
     assert len(obs_colonies_bleached) == 2
     assert obs_colonies_bleached[0]["attribute"] == str(benthic_attribute_2a1.id)
