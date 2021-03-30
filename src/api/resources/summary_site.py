a164a623270087a589b473a7c0db63a6b70c862e
Kim Fisher <kfisher@wcs.org>
2021-03-30 16:26:27 -0400

    tidy up

--- src/api/resources/summary_site.py
+++ src/api/resources/summary_site.py
@@ -7,7 +7,7 @@ from .base import (
 )
 from .sample_units import AggregatedViewMixin, BaseApiViewSet
 from ..permissions import UnauthenticatedReadOnlyPermission
-from api.models.view_models.summary_site import SummarySiteViewModel
+from ..models.view_models.summary_site import SummarySiteViewModel
 from ..models import Project
 from ..reports.fields import ReportField
 from ..reports.formatters import (
