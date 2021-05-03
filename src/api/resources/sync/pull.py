f12fffcc68e4ca3db75dbd5eab37c4fe6c74f6d2
Dustin Sampson <gridcell@gmail.com>
2021-05-03 08:31:35 -0700

    M1523: pull and push endpoints

---
+++ src/api/resources/sync/pull.py
@@ -0,0 +1,102 @@
+from django.core.exceptions import ObjectDoesNotExist
+
+
+def _get_model_subquery(model_class):
+    return str(model_class.objects.all().query)
+
+
+def _get_records(model_class, filters):
+    sub_query = _get_model_subquery(model_class)
+    pk_field_name = model_class._meta.pk.column
+
+    sql = f"""
+        WITH model_select AS (
+            {sub_query}
+        )
+        SELECT
+            model_select.*,
+            revision.record_id as revision_record_id,
+            revision.updated_on as revision_updated_on,
+            revision.revision_num as revision_revision_num,
+            revision.deleted as revision_deleted
+        FROM
+            revision
+        LEFT JOIN
+            model_select
+        ON
+            revision.record_id = model_select."{pk_field_name}"
+        WHERE
+            {" AND ".join(filters)}
+        ORDER BY
+            revision.updated_on DESC, revision.revision_num  DESC
+    """
+
+    return model_class.objects.raw(sql)
+
+
+def get_record(model_class, record_id):
+    filters = [f"revision.record_id = '{record_id}'::uuid"]
+    result = list(_get_records(model_class, filters))
+    if not result:
+        raise ObjectDoesNotExist()
+    
+    return result[0]
+
+
+def get_records(model_class, revision_num=None, project=None, profile=None):
+    table_name = model_class._meta.db_table
+    filters = [f"revision.table_name = '{table_name}'"]
+
+    if project is not None:
+        filters.append(f"revision.project_id = '{project}'::uuid")
+
+    if profile is not None:
+        filters.append(f"revision.profile_id = '{profile}'::uuid")
+
+    if revision_num is not None:
+        filters.append(f"revision.revision_num > {revision_num}")
+
+    return _get_records(model_class, filters)
+
+
+def serialize_revisions(serializer, record_set, skip_deletes=False):
+    last_rev_num = None
+    updates = []
+    deletes = []
+    for rec in record_set:
+        rev_num = rec.revision_revision_num
+
+        if last_rev_num is None or rev_num > last_rev_num:
+            last_rev_num = rev_num
+
+        if rec.revision_deleted is True and skip_deletes is False:
+            deletes.append(
+                {"id": str(rec.revision_record_id), "_last_revision_num": rev_num}
+            )
+            continue
+        elif rec.revision_deleted and skip_deletes is True:
+            continue
+
+        serialized_rec = serializer(rec, context={"request": None}).data
+        serialized_rec["_last_revision_num"] = rev_num
+        serialized_rec["_modified"] = False
+        serialized_rec["_deleted"] = False
+        updates.append(serialized_rec)
+
+    return {
+        "updates": updates,
+        "deletes": deletes,
+        "last_revision_num": last_rev_num,
+    }
+
+
+def get_serialized_records(viewset, revision_num=None, project=None, profile=None):
+
+    serializer = viewset.serializer_class
+    model_class = serializer.Meta.model
+
+    record_set = get_records(model_class, revision_num, project, profile)
+
+    return serialize_revisions(
+        serializer, record_set, skip_deletes=revision_num is None
+    )
---
+++ src/api/resources/sync/push.py
@@ -0,0 +1,85 @@
+from django.core.exceptions import ObjectDoesNotExist
+
+from api.models import Revision
+
+
+def get_request_method(record):
+    """Get what the request method based on the
+    record's properties.
+
+    :param record: Edits record.
+    :type record: dict
+    :return: Request method
+    :rtype: str
+    """
+    if record.get("_deleted") is True:
+        return "DELETE"
+    elif record.get("_last_revision_num") is None:
+        return "POST"
+    else:
+        return "PUT"
+
+
+def has_conflicts(record_id, last_revision_num):
+    """Check if record would have a conflict if edits
+    were applied.
+
+    :param record_id: Id of the record with edits.
+    :type record_id: str
+    :param last_revision_num:
+    :type last_revision_num: int or None
+    :return: True if there's a conflict.
+    :rtype: bool
+    """
+    if last_revision_num is None:
+        return False
+
+    rev = Revision.objects.get(record_id=record_id)
+    return rev.revision_num > last_revision_num
+
+
+def apply_changes(request, serializer, record, force=False):
+    """Create, update or delete record.
+
+    :param request: Generated django request or supplied ViewRequest created.  Request's method
+    should be set based on the edit being applied (PUT, POST, DELETE).
+    :type request: rest_framework.requests.Request or api.resources.sync.view.ViewRequest
+    :param serializer: Serializer for deserializing record. example: CollectRecordSerializer
+    :type serializer: rest_framework.serializers.ModelSerializer
+    :param record: Serialized record
+    :type record: dict
+    :param force: Ignore conflicts and apply change, defaults to False
+    :type force: bool, optional
+    :return: Status code and errors [optional]
+    :rtype: tuple
+    """
+    is_deleted = record.get("_deleted") is True
+    record_id = record.get("id")
+    model_class = serializer.Meta.model
+    if is_deleted:
+        try:
+            model_class.objects.get(pk=record_id).delete()
+        except ObjectDoesNotExist:
+            pass
+
+        return 204, None
+
+    instance = None
+    last_revision_num = record.get("_last_revision_num")
+    if has_conflicts(record_id, last_revision_num) and force is False:
+        return 409, None
+
+    if last_revision_num is not None:
+        instance = model_class.objects.get(pk=record_id)
+        status_code = 200
+    else:
+        status_code = 201
+
+    s = serializer(instance=instance, data=record, context={"request": request})
+
+    if s.is_valid() is False:
+        return 400, s.errors
+
+    s.save()
+
+    return status_code, None
