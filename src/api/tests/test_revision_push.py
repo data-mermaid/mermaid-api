a0e4cc4397cc711a519faa8a3c2dda8a5031bae6
Dustin Sampson <gridcell@gmail.com>
2021-05-04 08:10:44 -0700

    M1405: remove stale test

--- src/api/tests/test_revision_push.py
+++ src/api/tests/test_revision_push.py
@@ -2,23 +2,8 @@ import uuid
 
 from api.models import CollectRecord
 from api.resources.collect_record import CollectRecordSerializer
-from api.resources.sync.push import apply_changes, check_for_revision_conflicts
+from api.resources.sync.push import apply_changes
 from api.mocks import MockRequest
-
-
-def test_check_for_revision_conflicts(db_setup, serialized_tracked_collect_record):
-    serialized_collect_record = serialized_tracked_collect_record["updates"][0]
-    checks = check_for_revision_conflicts([serialized_collect_record])
-    assert checks[serialized_collect_record["id"]].has_conflict is False
-
-    serialized_collect_record = serialized_tracked_collect_record["updates"][0]
-    serialized_collect_record["_last_revision_num"] = 2
-    checks = check_for_revision_conflicts([serialized_collect_record])
-    assert checks[serialized_collect_record["id"]].has_conflict is True
-
-    new_collect_record = {"id": "508665bb-0f67-42e9-af52-6e500b52100f"}
-    checks = check_for_revision_conflicts([new_collect_record])
-    assert checks[new_collect_record["id"]].has_conflict is False
 
 
 def test_apply_changes(db_setup, serialized_tracked_collect_record, profile1, project1):
