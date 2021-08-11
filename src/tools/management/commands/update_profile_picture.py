eef94d6146a464c4c727fffac90740679f50d8f7
Dustin Sampson <gridcell@gmail.com>
2021-08-11 13:40:55 -0700

    M325: update profile picture DJ command

---
+++ src/tools/management/commands/update_profile_picture.py
@@ -0,0 +1,42 @@
+from time import sleep
+
+from api.models import Profile
+from api.utils.auth0utils import get_user_info
+from .progress_bar_base_command import ProgressBarBaseCommand
+
+
+class Command(ProgressBarBaseCommand):
+    help = "Update profile picture from Auth0 profile"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--throttle",
+            type=int,
+            default=5,
+            help="Number of auth0 profiles to fetch before 1 second break.",
+        )
+
+    def update_profile_picture(self, profile):
+        auth_user = profile.authusers.all()[0]
+        user_info = get_user_info(auth_user.user_id)
+
+        profile.picture_url = user_info["picture"]
+        profile.save()
+
+    def handle(self, *args, **options):
+        throttle = options["throttle"]
+        qry = Profile.objects.all()
+
+        num_profiles = qry.count()
+        self.draw_progress_bar(0)
+        n = 0
+        for profile in qry:
+            self.draw_progress_bar(float(n) / num_profiles)
+            self.update_profile_picture(profile)
+            if n == throttle - 1:
+                print("break")
+                sleep(1)
+                n = 0
+            n += 1
+
+        self.draw_progress_bar(1)
