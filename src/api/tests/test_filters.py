41816bd0cf1f553fa3712d61818cf5404c94b2e5
Dustin Sampson <gridcell@gmail.com>
2021-08-18 05:39:25 -0700

    M1570: SafeSearchFilter tests

---
+++ src/api/tests/test_filters.py
@@ -0,0 +1,12 @@
+
+
+def test_safe_search_filter(
+    db_setup, api_client1, base_project
+):
+    response = api_client1.get("/v1/sites/?search=site", format="json")
+    response_data = response.json()
+
+    assert response_data["count"] == 2
+
+    response = api_client1.get("/v1/sites/?search=(.*17.*|.*33.*|.*01.*|.*\\..*|.*68,.*|.*94.*|.*30.*|.*45.*|.*\\..*|.*62.*)", format="json")
+    assert response.status_code == 400
