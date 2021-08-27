49b5664843ee888074c6ed054870caab375f5e8c
Kim Fisher <kfisher@wcs.org>
2021-08-27 11:16:11 -0400

    emails refactored to use multipart templates consistently

--- src/api/utils/email.py
+++ src/api/utils/email.py
@@ -2,58 +2,48 @@ import datetime
 import os
 
 from django.conf import settings
-from django.core.mail import EmailMessage, EmailMultiAlternatives
+from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
 from ..decorators import run_in_thread
 from ..models.mermaid import ProjectProfile
 
 
-def mermaid_email(subject, heading, subheading, body, to, from_email=None):
-    SUBJECT_TMPL = u'[MERMAID] {subject}'
-    BODY_TMPL = u'''
-    <div style="text-align:center; background-color:#666; padding: 20px;">
-        <img style="width:auto; display:inline-block;" 
-        src="https://datamermaid.org/wp-content/themes/mermaid/img/mermaid_logo.png">
-    </div>
-    <h2>{heading}</h2>
-    <h3>{subheading}</h3>
-    <p>{timestamp} UTC</p>
-    {body}
-    '''
+@run_in_thread
+def mermaid_email(
+    subject, template, to, context=None, from_email=settings.DEFAULT_FROM_EMAIL
+):
+    _subject = f"[MERMAID] {subject}"
+    path, _ = os.path.splitext(template)
+    template_html = f"{path}.html"
+    template_text = f"{path}.txt"
 
-    subject = SUBJECT_TMPL.format(subject=subject)
-    timestamp = datetime.datetime.utcnow().replace(microsecond=0).isoformat()
-    content = BODY_TMPL.format(heading=heading, subheading=subheading, timestamp=timestamp, body=body)
-    from_email = from_email or settings.DEFAULT_FROM_EMAIL
+    context = context or {}
+    context["timestamp"] = datetime.datetime.utcnow().replace(microsecond=0).isoformat()
+    text_content = render_to_string(template_text, context=context)
+    html_content = render_to_string(template_html, context=context)
 
-    email_args = dict(
-        subject=subject,
-        body=content,
-        to=to,
-        reply_to=[from_email]
-    )
     if settings.ENVIRONMENT == "prod":
-        message = EmailMessage(**email_args)
-        message.content_subtype = 'html'
-        message.send()
+        msg = EmailMultiAlternatives(
+            _subject, text_content, from_email, to=[to], reply_to=[from_email]
+        )
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
     else:
-        print(email_args)
+        print(text_content)
 
 
-
-def email_project_admins(project, subject, body, from_email=None):
+def email_project_admins(project, subject, template, context):
     project_admins = ProjectProfile.objects.filter(
-        project_id=project, role=ProjectProfile.ADMIN).select_related('profile')
+        project_id=project, role=ProjectProfile.ADMIN
+    ).select_related("profile")
 
     if project_admins.count() > 0:
         mermaid_email(
-            subject=subject,
-            heading=project.name,
-            subheading='MERMAID Project Administrator Communication',
-            body=body,
-            to=[p.profile.email for p in project_admins],
-            from_email=[from_email]
+            subject,
+            template,
+            [p.profile.email for p in project_admins],
+            context=context,
         )
 
 
@@ -62,26 +52,3 @@ def contact_project_admins(project, subject, body, from_email):
     # lots of checking/validation/antispam here
     # email_project_admins(project, subject, body, from_email)
     pass
-
-
-@run_in_thread
-def mermaid_email_from_template(subject, template, to, data=None, from_email=None):
-    _subject = f"[MERMAID] {subject}"
-    path, _ = os.path.splitext(template)
-    template_html = f"{path}.html"
-    template_text = f"{path}.txt"
-    text_content = render_to_string(template_text, context=data)
-    html_content = render_to_string(template_html, context=data)
-
-    if settings.ENVIRONMENT == "prod":
-        msg = EmailMultiAlternatives(
-            _subject,
-            text_content,
-            from_email,
-            to=[to],
-            reply_to=[from_email]
-        )
-        msg.attach_alternative(html_content, "text/html")
-        msg.send()
-    else:
-        print(text_content)
