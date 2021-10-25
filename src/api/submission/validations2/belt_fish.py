150744040c7dce2f528a1908210e58a2e6c9ed0a
Dustin Sampson <gridcell@gmail.com>
2021-10-25 08:01:36 -0700

    M1586: belt fish validations

---
+++ src/api/submission/validations2/belt_fish.py
@@ -0,0 +1,167 @@
+# from ...resources.belt_fish import BeltFishSerializer
+from .base import (
+    FIELD_LEVEL,
+    LIST_VALIDATION_TYPE,
+    RECORD_LEVEL,
+    ROW_LEVEL,
+    VALUE_VALIDATION_TYPE,
+    Validation,
+)
+from .validators import (
+    AllEqualValidator,
+    BiomassValidator,
+    DepthValidator,
+    FishCountValidator,
+    FishFamilySubsetValidator,
+    LenSurveyedValidator,
+    ObservationCountValidator,
+    RequiredValidator,
+    SampleDateValidator,
+    SampleTimeValidator,
+    # SerializerValidator,
+    TotalFishCountValidator,
+    UniqueManagementValidator,
+    UniqueSiteValidator,
+    UniqueTransectValidator,
+)
+
+belt_fish_validations = [
+    Validation(
+        validator=SampleDateValidator(
+            sample_date_path="data.sample_event.sample_date",
+            sample_time_path="data.fishbelt_transect.sample_time",
+            site_path="data.sample_event.site",
+        ),
+        paths=["data.sample_event.sample_date"],
+        validation_level=FIELD_LEVEL,
+        validation_type=VALUE_VALIDATION_TYPE,
+    ),
+    Validation(
+        validator=SampleTimeValidator(
+            sample_time_path="data.fishbelt_transect.sample_time",
+        ),
+        paths=["data.fishbelt_transect.sample_time"],
+        validation_level=FIELD_LEVEL,
+        validation_type=VALUE_VALIDATION_TYPE,
+    ),
+    Validation(
+        validator=LenSurveyedValidator(
+            len_surveyed_path="data.fishbelt_transect.len_surveyed",
+        ),
+        paths=["data.fishbelt_transect.len_surveyed"],
+        validation_level=FIELD_LEVEL,
+        validation_type=VALUE_VALIDATION_TYPE,
+    ),
+    Validation(
+        validator=FishFamilySubsetValidator(
+            observations_path="data.obs_belt_fishes",
+            site_path="data.sample_event.site",
+        ),
+        paths=["data.obs_belt_fishes"],
+        validation_level=ROW_LEVEL,
+        validation_type=LIST_VALIDATION_TYPE,
+    ),
+    Validation(
+        validator=FishCountValidator(
+            observations_path="data.obs_belt_fishes", observation_count_path="count"
+        ),
+        paths=["data.obs_belt_fishes"],
+        validation_level=ROW_LEVEL,
+        validation_type=LIST_VALIDATION_TYPE,
+    ),
+    Validation(
+        validator=TotalFishCountValidator(
+            observations_path="data.obs_belt_fishes", observation_count_path="count"
+        ),
+        paths=["data.obs_belt_fishes"],
+        validation_level=RECORD_LEVEL,
+        validation_type=VALUE_VALIDATION_TYPE,
+    ),
+    Validation(
+        validator=ObservationCountValidator(observations_path="data.obs_belt_fishes"),
+        paths=["data.obs_belt_fishes"],
+        validation_level=RECORD_LEVEL,
+        validation_type=VALUE_VALIDATION_TYPE,
+    ),
+    Validation(
+        validator=BiomassValidator(
+            observations_path="data.obs_belt_fishes",
+            len_surveyed_path="data.fishbelt_transect.len_surveyed",
+            width_path="data.fishbelt_transect.width",
+            obs_fish_attribute_path="fish_attribute",
+            obs_count_path="count",
+            obs_size_path="size",
+        ),
+        paths=["data.obs_belt_fishes"],
+        validation_level=RECORD_LEVEL,
+        validation_type=VALUE_VALIDATION_TYPE,
+    ),
+    Validation(
+        validator=UniqueTransectValidator(
+            protocol="fishbelt",
+            label_path="data.fishbelt_transect.label",
+            number_path="data.fishbelt_transect.number",
+            width_path="data.fishbelt_transect.width",
+            relative_depth_path="data.fishbelt_transect.relative_depth_path",
+            site_path="data.sample_event.site",
+            management_path="data.sample_event.management",
+            sample_date_path="data.sample_event.sample_date",
+            depth_path="data.fishbelt_transect.depth",
+        ),
+        paths=[
+            "data.fishbelt_transect.label",
+            "data.fishbelt_transect.number",
+            "data.fishbelt_transect.width",
+            "data.fishbelt_transect.relative_depth",
+            "data.fishbelt_transect.depth",
+            "data.sample_event.site",
+            "data.sample_event.management",
+            "data.sample_event.sample_date",
+        ],
+        validation_level=RECORD_LEVEL,
+        validation_type=VALUE_VALIDATION_TYPE,
+    ),
+    Validation(
+        validator=UniqueSiteValidator(
+            site_path="data.sample_event.site",
+        ),
+        paths=["data.sample_event.site"],
+        validation_level=FIELD_LEVEL,
+        validation_type=VALUE_VALIDATION_TYPE,
+    ),
+    Validation(
+        validator=UniqueManagementValidator(
+            management_path="data.sample_event.management",
+        ),
+        paths=["data.sample_event.management"],
+        validation_level=FIELD_LEVEL,
+        validation_type=VALUE_VALIDATION_TYPE,
+    ),
+    Validation(
+        validator=RequiredValidator(path="data.observers"),
+        paths=["data.observers"],
+        validation_level=FIELD_LEVEL,
+        validation_type=VALUE_VALIDATION_TYPE,
+    ),
+    Validation(
+        validator=AllEqualValidator(path="data.obs_belt_fishes"),
+        paths=["data.obs_belt_fishes"],
+        validation_level=RECORD_LEVEL,
+        validation_type=VALUE_VALIDATION_TYPE,
+    ),
+    Validation(
+        validator=DepthValidator(depth_path="data.fishbelt_transect.depth"),
+        paths=["data.fishbelt_transect.depth"],
+        validation_level=FIELD_LEVEL,
+        validation_type=VALUE_VALIDATION_TYPE,
+    ),
+    # Validation(
+    #     validator=SerializerValidator(
+    #         serializer=BeltFishSerializer
+    #     ),
+    #     paths=["."],
+    #     validation_level=RECORD_LEVEL,
+    #     validation_type=VALUE_VALIDATION_TYPE,
+    #     requires_instance=True,
+    # ),
+]
---
+++ src/api/submission/validations2/validators/__init__.py
@@ -0,0 +1,15 @@
+# flake8: noqa
+from .base import ERROR, IGNORE, OK, WARN, BaseValidator, ValidatorResult
+from .biomass import BiomassValidator
+from .depth import DepthValidator
+from .fish_count import FishCountValidator, TotalFishCountValidator
+from .fish_family import FishFamilySubsetValidator
+from .generic import AllEqualValidator, RequiredValidator
+from .len_surveyed import LenSurveyedValidator
+from .management import UniqueManagementValidator
+from .observations import ObservationCountValidator
+from .sample_date import SampleDateValidator
+from .sample_time import SampleTimeValidator
+from .serializer import SerializerValidator
+from .site import UniqueSiteValidator
+from .transect import UniqueTransectValidator
