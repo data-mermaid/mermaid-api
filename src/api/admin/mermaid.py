7c6706619e03f5d803f1eeda9af9e07b1bba8803
Dustin Sampson <gridcell@gmail.com>
2021-12-20 07:16:37 -0800

    M795: benthic photo quadrat transect admin

--- src/api/admin/mermaid.py
+++ src/api/admin/mermaid.py
@@ -1010,3 +1010,69 @@ class CovariateAdmin(BaseAdmin):
 @admin.register(AuditRecord)
 class AuditRecordAdmin(BaseAdmin):
     list_display = ("event_on", "event_type", "model", "record_id")
+
+
+class ObsBenthicPhotoQuadratInline(ObservationInline):
+    model = ObsBenthicPhotoQuadrat
+    cache_fields = ["attribute", "growth_form"]
+
+
+@admin.register(BenthicPhotoQuadratTransect)
+class BenthicPhotoQuadratTransectAdmin(BaseAdmin):
+    list_display = ("name", "quadrat_size", "depth")
+    inlines = (
+        ObserverInline,
+        ObsBenthicPhotoQuadratInline,
+    )
+    readonly_fields = ["created_by", "updated_by", "cr_id"]
+    search_fields = [
+        "quadrat_transect__sample_event__site__name",
+        "quadrat_transect__sample_event__sample_date",
+        "quadrat_transect__sample_event__site__project__name",
+    ]
+    ordering = ["quadrat_transect__sample_event__site__name"]
+
+    def name(self, obj):
+        return str(obj.quadrat)
+
+    name.admin_order_field = "quadrat_transect__sample_event__site__name"
+
+    def quadrat_size(self, obj):
+        return obj.quadrat_transect.quadrat_size
+    
+    quadrat_size.admin_order_field = "quadrat_transect__quadrat_size"
+
+    def depth(self, obj):
+        return obj.transect.depth
+
+    depth.admin_order_field = "quadrat_transect__sample_event__depth"
+
+    def cr_id(self, obj):
+        return obj.quadrat_transect.collect_record_id
+
+    cr_id.short_description = "CollectRecord ID"
+
+    def get_queryset(self, request):
+        return (
+            super()
+            .get_queryset(request)
+            .select_related(
+                "created_by",
+                "updated_by",
+                "quadrat_transect",
+                "quadrat_transect__sample_event",
+                "quadrat_transect__sample_event__site",
+            )
+        )
+
+    def get_formsets_with_inlines(self, request, obj=None):
+        attributes = BenthicAttribute.objects.none()
+        growth_forms = GrowthForm.objects.none()
+        if obj is not None:
+            attributes = BenthicAttribute.objects.only("pk", "name").order_by("name")
+            growth_forms = GrowthForm.objects.all()
+
+        for inline in self.get_inline_instances(request, obj):
+            inline.cached_attributes = [(a.pk, a.name) for a in attributes]
+            inline.cached_growth_forms = [(gf.pk, gf.name) for gf in growth_forms]
+            yield inline.get_formset(request, obj), inline
