d97bd10b68df91802a65a12febac46da78346e25
Dustin Sampson <dustin@sparkgeo.com>
2022-03-11 07:01:53 -0800

    wip

--- src/api/submission/validations2/validators/obs_benthic_photo_quadrat.py
+++ src/api/submission/validations2/validators/obs_benthic_photo_quadrat.py
@@ -46,3 +46,41 @@ class PointsPerQuadratValidator(BaseValidator):
             return WARN, self.INVALID_NUMBER_POINTS, {"invalid_quadrat_numbers": invalid_quadrat_numbers}
 
         return OK
+
+
+class UniqueObsBenthicPhotoQuadrat(BaseValidator):
+    # "benthic_photo_quadrat_transect",
+    #         "quadrat_number",
+    #         "attribute",
+    #         "growth_form"
+    def __init__(
+        self,
+        obs_benthic_photo_quadrats_path,
+        observation_quadrat_number_path,
+        observation_attribute_path,
+        observation_growth_form_path,
+        **kwargs
+    ):
+        self.obs_benthic_photo_quadrats_path = obs_benthic_photo_quadrats_path
+        self.observation_quadrat_number_path = observation_quadrat_number_path
+        self.observation_attribute_path = observation_attribute_path
+        self.observation_growth_form_path = observation_growth_form_path
+        super().__init__(**kwargs)
+    
+    def _create_key(self, obs):
+        quadrat_number = self.get_value(obs, self.observation_quadrat_number_path)
+        attribute = self.get_value(obs, self.observation_attribute_path)
+        growth_form = self.get_value(obs, self.observation_growth_form_path)
+        return f"{quadrat_number}::{attribute}::{growth_form}"
+
+    @validator_result
+    def __call__(self, collect_record, **kwargs):
+        observations = self.get_value(collect_record, self.obs_benthic_photo_quadrats_path) or []
+        groups = defaultdict(list)
+        for obs in observations:
+            key = self._create_key(obs)
+            groups[key].append(obs.get("id"))
+        
+        duplicates = [group for group in groups if len(group) > 1]
+
+        return OK
