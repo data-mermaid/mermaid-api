0b98a8363267e0c8162ef26ba2bb826438089801
Kim Fisher <kfisher@wcs.org>
2022-04-08 14:56:01 -0400

    email fixes

--- src/api/utils/email.py
+++ src/api/utils/email.py
@@ -11,7 +11,7 @@ from ..models.mermaid import ProjectProfile
 
 @run_in_thread
 def mermaid_email(
-    subject, template, to, context=None, from_email=settings.DEFAULT_FROM_EMAIL
+    subject, template, to, context=None, from_email=None, reply_to=None
 ):
     _subject = f"[MERMAID] {subject}"
     path, _ = os.path.splitext(template)
@@ -22,10 +22,12 @@ def mermaid_email(
     context["timestamp"] = datetime.datetime.utcnow().replace(microsecond=0).isoformat()
     text_content = render_to_string(template_text, context=context)
     html_content = render_to_string(template_html, context=context)
+    from_email = from_email or settings.DEFAULT_FROM_EMAIL
+    reply_to = reply_to or [settings.DEFAULT_FROM_EMAIL]
 
     if settings.ENVIRONMENT == "prod":
         msg = EmailMultiAlternatives(
-            _subject, text_content, from_email, to=to, reply_to=[from_email]
+            _subject, text_content, to=to, from_email=from_email, reply_to=reply_to
         )
         msg.attach_alternative(html_content, "text/html")
         msg.send()
@@ -33,22 +35,26 @@ def mermaid_email(
         print(text_content)
 
 
-def email_project_admins(project, subject, template, context):
+def email_project_admins(project, subject, template, context, from_email=None):
     project_admins = ProjectProfile.objects.filter(
         project_id=project, role=ProjectProfile.ADMIN
     ).select_related("profile")
+    project_admin_emails = [p.profile.email for p in project_admins]
+    from_email = from_email or settings.DEFAULT_FROM_EMAIL
 
     if project_admins.count() > 0:
         mermaid_email(
             subject,
             template,
-            [p.profile.email for p in project_admins],
+            project_admin_emails,
             context=context,
+            from_email=from_email,
+            reply_to=project_admin_emails
         )
 
 
 # for hooking up to https://api.datamermaid.org/contact_project?project_id=2c56b92b-ba1c-491f-8b62-23b1dc728890
 def contact_project_admins(project, subject, body, from_email):
     # lots of checking/validation/antispam here
-    # email_project_admins(project, subject, body, from_email)
+    # email_project_admins(project, subject, body, from_email=from_email)
     pass
