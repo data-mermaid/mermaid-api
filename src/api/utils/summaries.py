6734731d9a50d07d54e0034e1d874ddd31261204
Dustin Sampson <gridcell@gmail.com>
2022-04-12 13:33:34 -0700

    M1652: Fix Summary Sample Event Update There's a race condition when multiple updates occur for a project at the same time - Process has been moved to transaction - Updates fetch before deletes

--- src/api/utils/summaries.py
+++ src/api/utils/summaries.py
@@ -1,3 +1,5 @@
+from django.db import transaction
+
 from ..models import (
     Project,
     SummarySampleEventModel,
@@ -19,13 +21,15 @@ def update_project_summary_site(project_id, skip_test_project=True):
     ):
         return
 
-    SummarySiteModel.objects.filter(project_id=project_id).delete()
-    for record in SummarySiteSQLModel.objects.all().sql_table(project_id=project_id):
-        values = {
-            field.name: getattr(record, field.name)
-            for field in SummarySiteModel._meta.fields
-        }
-        SummarySiteModel.objects.create(**values)
+    with transaction.atomic():
+        summary_sites = list(SummarySiteSQLModel.objects.all().sql_table(project_id=project_id))
+        SummarySiteModel.objects.filter(project_id=project_id).delete()
+        for record in summary_sites:
+            values = {
+                field.name: getattr(record, field.name)
+                for field in SummarySiteModel._meta.fields
+            }
+            SummarySiteModel.objects.create(**values)
 
 
 def update_project_summary_sample_event(project_id, skip_test_project=True):
@@ -35,12 +39,14 @@ def update_project_summary_sample_event(project_id, skip_test_project=True):
     ):
         return
 
-    SummarySampleEventModel.objects.filter(project_id=project_id).delete()
-    for record in SummarySampleEventSQLModel.objects.all().sql_table(
-        project_id=project_id
-    ):
-        values = {
-            field.name: getattr(record, field.name)
-            for field in SummarySampleEventModel._meta.fields
-        }
-        SummarySampleEventModel.objects.create(**values)
+    with transaction.atomic():
+        summary_sample_events = list(SummarySampleEventSQLModel.objects.all().sql_table(
+            project_id=project_id
+        ))
+        SummarySampleEventModel.objects.filter(project_id=project_id).delete()
+        for record in summary_sample_events:
+            values = {
+                field.name: getattr(record, field.name)
+                for field in SummarySampleEventModel._meta.fields
+            }
+            SummarySampleEventModel.objects.create(**values)
