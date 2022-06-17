00a847b2dd32ec29d0ec1a951d89c2d5fec0609d
Dustin Sampson <dustin@sparkgeo.com>
2022-06-17 16:40:13 -0700

    Tests for copy function

--- src/api/tests/test_project.py
+++ src/api/tests/test_project.py
@@ -1,6 +1,8 @@
+from venv import create
 from django.urls import reverse
 
 from api.models import Profile, ProjectProfile
+from api.utils.project import copy_project_and_resources, create_project
 
 
 def test_add_profile_new_user(
@@ -98,3 +100,32 @@ def test_project_summary(
     sample_unit_methods = site_collecting_summary[site1_id]["sample_unit_methods"]
     assert "fishbelt" in sample_unit_methods
     assert str(profile1.pk) in sample_unit_methods["fishbelt"]["profile_summary"]
+
+
+def test_copy_project_and_resources(
+    project1,
+    project_profile1,
+    project_profile2,
+    site1,
+    site2,
+    site3,
+    management1,
+    management2
+):
+    project_name = "new title"
+    owner_profile = project_profile1.profile
+    new_project = copy_project_and_resources(owner_profile, project_name, project1.pk)
+
+    assert new_project.pk != project1.pk
+    assert new_project.name == project_name
+
+    original_tags = [t.name for t in project1.tags.all()]
+    assert new_project.tags.filter(name__in=original_tags).count() == len(original_tags)
+
+    assert new_project.tags.count() == 3    
+
+    assert new_project.sites.all().count() == 3
+    assert new_project.management_set.count() == 2
+
+    assert new_project.profiles.get(profile=owner_profile).role == ProjectProfile.ADMIN
+    assert new_project.profiles.get(profile=project_profile2.profile).role == project_profile2.role
