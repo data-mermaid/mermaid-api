ce5bee3d4ed579e60b79c3dc4e473abef807d470
Dustin Sampson <dustin@sparkgeo.com>
2022-06-21 13:07:21 -0700

    fix indentation issue with project utils

--- src/api/utils/project.py
+++ src/api/utils/project.py
@@ -198,29 +198,29 @@ def copy_project_and_resources(owner_profile, new_project_name, original_project
         profile=owner_profile
     )
 
-        project_profiles = []
-        for pp in original_project.profiles.filter(~Q(profile=owner_profile)):
-            pp.id = None
-            pp.project = new_project
-            project_profiles.append(pp)
-        
-        ProjectProfile.objects.bulk_create(project_profiles)
+    project_profiles = []
+    for pp in original_project.profiles.filter(~Q(profile=owner_profile)):
+        pp.id = None
+        pp.project = new_project
+        project_profiles.append(pp)
     
-        new_sites = []
-        for site in original_project.sites.all():
-            site.id = None
-            site.project = new_project
-            new_sites.append(site)
-        
-        Site.objects.bulk_create(new_sites)
-        
-        new_management_regimes = []
-        for mr in original_project.management_set.all():
-            mr.id = None
-            mr.project = new_project
-            new_management_regimes.append(mr)
-        
-        Management.objects.bulk_create(new_management_regimes)
+    ProjectProfile.objects.bulk_create(project_profiles)
+
+    new_sites = []
+    for site in original_project.sites.all():
+        site.id = None
+        site.project = new_project
+        new_sites.append(site)
+    
+    Site.objects.bulk_create(new_sites)
+    
+    new_management_regimes = []
+    for mr in original_project.management_set.all():
+        mr.id = None
+        mr.project = new_project
+        new_management_regimes.append(mr)
+    
+    Management.objects.bulk_create(new_management_regimes)
 
     return new_project
 
