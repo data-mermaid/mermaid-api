b9ca77f772bb89ab622948f1fedaadd6daa32c64
Mike Connor <mike@sparkgeo.com>
2022-07-12 12:29:04 -0700

    Change gunicorn access and error log to stdout.

--- src/entry.sh
+++ src/entry.sh
@@ -1,12 +1,15 @@
 echo "Create Database, if not exists"
-# createdb -h $DB_HOST -p $DB_PORT -U $DB_USER $DB_NAME
-echo "SELECT 'CREATE DATABASE mermaid' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'mermaid')\gexec" | psql -h $DB_HOST -p $DB_PORT -U $DB_USER
+createdb -h $DB_HOST -p $DB_PORT -U $DB_USER $DB_NAME
+# echo "SELECT 'CREATE DATABASE mermaid' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${DB_NAME}')\gexec" | psql -h $DB_HOST -p $DB_PORT -U $DB_USER
 
-# set -e
+set -e
 
 echo "Starting Django Migrations"
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 echo "Run Gunicorn Server"
-gunicorn app.wsgi --bind 0.0.0.0:8080
+gunicorn app.wsgi \
+    --bind 0.0.0.0:8080 \
+    --access-logfile "-" \
+    --error-logfile "-" 
