e4bf74e968741dcbc77dd713a0cc22626eb0c36e
Alain St. Pierre <alain@aspcanada.com>
2022-09-06 16:32:43 -0700

    Allow recaptcha testing on local machine w/o secret key

--- src/app/settings.py
+++ src/app/settings.py
@@ -235,7 +235,7 @@ GEO_PRECISION = 6  # to nearest 10 cm
 CORAL_ATLAS_APP_ID = os.environ.get("CORAL_ATLAS_APP_ID")
 # https://github.com/llybin/drf-recaptcha
 DRF_RECAPTCHA_SECRET_KEY = os.environ.get("DRF_RECAPTCHA_SECRET_KEY")
-# DRF_RECAPTCHA_TESTING = True
+DRF_RECAPTCHA_TESTING = os.environ.get("DRF_RECAPTCHA_TESTING") or False
 
 # ************
 # ** CLIENT **
