1fb46138f82aa8af8d23308ba79b7f8c25ea58d6
Mike Connor <mike@sparkgeo.com>
2022-09-15 15:20:14 -0700

    Project tags jank.
    (cherry picked from commit 32b7ecf0dbb130ad5919edf30bb6a799227b80eb)

--- src/api/resources/project_tag.py
+++ src/api/resources/project_tag.py
@@ -1,5 +1,5 @@
 from .base import BaseApiViewSet, BaseAPISerializer, BaseAPIFilterSet
-from django.contrib.contenttypes.models import ContentType
+# from django.contrib.contenttypes.models import ContentType
 from ..models import Tag
 from ..permissions import UnauthenticatedReadOnlyPermission
 
@@ -23,9 +23,10 @@ class ProjectTagViewSet(BaseApiViewSet):
     permission_classes = [UnauthenticatedReadOnlyPermission]
     filterset_class = ProjectTagFilterSet
     serializer_class = ProjectTagSerializer
-    pt = ContentType.objects.get(app_label="api", model="project")
+    # pt = ContentType.objects.get(app_label="api", model="project")
     queryset = (
-        Tag.objects.filter(tagged_items__content_type_id=pt.pk)
+        Tag.objects
+        # .filter(tagged_items__content_type_id=pt.pk)
         .distinct()
         .order_by("name")
     )
