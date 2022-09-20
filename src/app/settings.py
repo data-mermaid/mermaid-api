8d7748d33ae8f5923961b49a0e779975f61d0f39
Alain St. Pierre <alain@aspcanada.com>
2022-09-20 13:36:11 -0700

    allow runserver in development to bypass django's staticfiles and use whitenoise instead

--- src/app/settings.py
+++ src/app/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'maintenance_mode',
