a30f59dc1dace08533a4c321a713d1d53e75645c
Kim Fisher <kfisher@wcs.org>
2022-10-07 16:41:10 -0400

    refactor out non-signal notifications code

--- src/api/notifications/__init__.py
+++ src/api/notifications/__init__.py
@@ -1,6 +1,79 @@
 from collections import defaultdict
-from ..models import CollectRecord, Notification
+from django.conf import settings
+from ..models import CollectRecord, Notification, ProjectProfile
+from ..utils.email import mermaid_email
 from ..utils.notification import add_notification
+
+
+def notify_project_admins(
+    project, subject, email_template, notify_template, context, from_email=None
+):
+    project_admins = ProjectProfile.objects.filter(
+        project_id=project, role=ProjectProfile.ADMIN
+    ).select_related("profile")
+    project_admin_profiles = [p.profile for p in project_admins]
+
+    if project_admins.count() > 0:
+        add_notification(
+            subject, Notification.INFO, notify_template, context, project_admin_profiles
+        )
+
+        project_admin_emails = [p.email for p in project_admin_profiles]
+        from_email = from_email or settings.DEFAULT_FROM_EMAIL
+        mermaid_email(
+            subject,
+            email_template,
+            project_admin_emails,
+            context=context,
+            from_email=from_email,
+            reply_to=project_admin_emails,
+        )
+
+
+def notify_admins_project_change(instance, text_changes):
+    subject = f"Changes to {instance.name}"
+    collect_project_url = (
+        f"https://{settings.DEFAULT_DOMAIN_COLLECT}/#/projects/{instance.pk}/details"
+    )
+
+    context = {
+        "project_name": instance.name,
+        "profile": instance.updated_by,
+        "collect_project_url": collect_project_url,
+        "text_changes": text_changes,
+    }
+    email_template = "emails/admins_project_change.html"
+    notify_template = "notifications/admins_project_change.txt"
+
+    notify_project_admins(instance, subject, email_template, notify_template, context)
+
+
+def notify_admins_change(instance, changetype):
+    if changetype == "add":
+        subject_snippet = "added to"
+        body_snippet = "given administrative privileges to"
+    elif changetype == "remove":
+        subject_snippet = "removed from"
+        body_snippet = "removed from this project, or is no longer an administrator for"
+    else:
+        return
+
+    subject = f"Project administrator {subject_snippet} {instance.project.name}"
+    collect_project_url = f"https://{settings.DEFAULT_DOMAIN_COLLECT}/#/projects/{instance.project.pk}/users"
+
+    context = {
+        "project_name": instance.project.name,
+        "profile": instance.profile,
+        "admin_profile": instance.updated_by,
+        "collect_project_url": collect_project_url,
+        "body_snippet": body_snippet,
+    }
+    email_template = "emails/admins_admins_change.html"
+    notify_template = "notifications/admins_admins_change.txt"
+
+    notify_project_admins(
+        instance.project, subject, email_template, notify_template, context
+    )
 
 
 def notify_cr_owners_site_mr_deleted(instance, deleted_by_user):
--- src/api/signals/notifications.py
+++ src/api/signals/notifications.py
@@ -1,18 +1,12 @@
 from django import urls
 from django.conf import settings
-from django.db.models.signals import post_delete, post_save, m2m_changed, pre_delete
+from django.db.models.signals import post_delete, post_save, m2m_changed
 from django.dispatch import receiver
 
-from ..models import (
-    BaseAttributeModel,
-    Tag,
-    ProjectProfile,
-    Notification,
-    Project,
-)
+from ..models import BaseAttributeModel, Tag, ProjectProfile, Project
+from ..notifications import notify_admins_change, notify_admins_project_change
 from ..utils import get_subclasses
 from ..utils.email import mermaid_email
-from ..utils.notification import add_notification
 
 
 __all__ = (
@@ -69,49 +63,6 @@ post_save.connect(
 )
 
 
-def notify_project_admins(
-    project, subject, email_template, notify_template, context, from_email=None
-):
-    project_admins = ProjectProfile.objects.filter(
-        project_id=project, role=ProjectProfile.ADMIN
-    ).select_related("profile")
-    project_admin_profiles = [p.profile for p in project_admins]
-
-    if project_admins.count() > 0:
-        add_notification(
-            subject, Notification.INFO, notify_template, context, project_admin_profiles
-        )
-
-        project_admin_emails = [p.email for p in project_admin_profiles]
-        from_email = from_email or settings.DEFAULT_FROM_EMAIL
-        mermaid_email(
-            subject,
-            email_template,
-            project_admin_emails,
-            context=context,
-            from_email=from_email,
-            reply_to=project_admin_emails,
-        )
-
-
-def notify_admins_project_change(instance, text_changes):
-    subject = f"Changes to {instance.name}"
-    collect_project_url = (
-        f"https://{settings.DEFAULT_DOMAIN_COLLECT}/#/projects/{instance.pk}/details"
-    )
-
-    context = {
-        "project_name": instance.name,
-        "profile": instance.updated_by,
-        "collect_project_url": collect_project_url,
-        "text_changes": text_changes,
-    }
-    email_template = "emails/admins_project_change.html"
-    notify_template = "notifications/admins_project_change.txt"
-
-    notify_project_admins(instance, subject, email_template, notify_template, context)
-
-
 @receiver(post_save, sender=Project)
 def notify_admins_project_instance_change(sender, instance, created, **kwargs):
     if created or not hasattr(instance, "_old_values"):
@@ -155,34 +106,6 @@ def notify_admins_project_tags_change(
                 text_changes.append(f"{verb} organization: {t.name}")
 
             notify_admins_project_change(instance, text_changes)
-
-
-def notify_admins_change(instance, changetype):
-    if changetype == "add":
-        subject_snippet = "added to"
-        body_snippet = "given administrative privileges to"
-    elif changetype == "remove":
-        subject_snippet = "removed from"
-        body_snippet = "removed from this project, or is no longer an administrator for"
-    else:
-        return
-
-    subject = f"Project administrator {subject_snippet} {instance.project.name}"
-    collect_project_url = f"https://{settings.DEFAULT_DOMAIN_COLLECT}/#/projects/{instance.project.pk}/users"
-
-    context = {
-        "project_name": instance.project.name,
-        "profile": instance.profile,
-        "admin_profile": instance.updated_by,
-        "collect_project_url": collect_project_url,
-        "body_snippet": body_snippet,
-    }
-    email_template = "emails/admins_admins_change.html"
-    notify_template = "notifications/admins_admins_change.txt"
-
-    notify_project_admins(
-        instance.project, subject, email_template, notify_template, context
-    )
 
 
 @receiver(post_save, sender=ProjectProfile)
