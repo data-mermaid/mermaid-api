dc7ca1081a0f5ae78b3575a73a3c338a8ea41f14
Alain St. Pierre <alain@aspcanada.com>
2022-11-16 15:28:48 -0800

    just use ONE combined SSL cert

--- iac/stacks/common.py
+++ iac/stacks/common.py
@@ -155,19 +155,11 @@ class CommonStack(Stack):
             certificate_arn=f"arn:aws:acm:us-east-1:{self.account}:certificate/783d7a91-1ebd-4387-9518-e28521086db6"
         )
 
-        # Create a cert for the API zone
-        api_cert = acm.Certificate(
-            self,
-            "APISSLCert",
-            domain_name=f"*.{api_domain}",
-            validation=acm.CertificateValidation.from_dns(self.api_zone)
-        )
-
         self.load_balancer.add_listener(
             id="MermaidApiListener",
             protocol=elb.ApplicationProtocol.HTTPS,
             default_action=elb.ListenerAction.fixed_response(404),
-            certificates=[default_cert, api_cert]
+            certificates=[default_cert]
         )
         # self.load_balancer.add_redirect() # Needs to be HTTPs first.
 
