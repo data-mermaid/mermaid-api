f79d7030238bbc53bd01a1fc42b7d33a4af206bd
Dustin Sampson <dustin@sparkgeo.com>
2022-12-09 13:52:12 -0800

    Revert "wip"
    This reverts commit 9dd1aa69486b6b600c3204d6c167711ac1e8c00c.

--- iac/app.py
+++ iac/app.py
@@ -3,7 +3,6 @@ import os
 from aws_cdk import App, Environment
 from iac.stacks.common import CommonStack
 from iac.stacks.api import ApiStack
-from iac.stacks.static_site import StaticSiteStack
 from iac.settings.dev import DEV_SETTINGS
 from iac.settings.prod import PROD_SETTINGS
 
@@ -54,40 +53,6 @@ prod_api_stack = ApiStack(
     load_balancer=common_stack.load_balancer,
     container_security_group=common_stack.ecs_sg,
     api_zone=common_stack.api_zone,
-)
-
-
-
-# domain_name
-# sub_domain_name
-# enable_s3_website_endpoint
-# domain_certificate_arn
-# hosted_zone_id
-# hosted_zone_name
-
-props = {
-    "namespace": "mermaid",
-    "domain_name": "datamermaid.org",
-    "sub_domain_name": "dev-public",
-    "domain_certificate_arn": app.node.try_get_context(
-        "domain_certificate_arn"
-    ),
-    "enable_s3_website_endpoint": app.node.try_get_context(
-        "enable_s3_website_endpoint"
-    ),
-    "origin_custom_header_parameter_name": app.node.try_get_context(
-        "origin_custom_header_parameter_name"
-    ),
-    "hosted_zone_id": app.node.try_get_context("hosted_zone_id"),
-    "hosted_zone_name": app.node.try_get_context("hosted_zone_name"),
-}
-
-StaticSite = StaticSiteStack(
-    scope=app,
-    construct_id="dev-mermaid-public-site",
-    props=props,
-    env=cdk_env,
-    description="MERMAID dev public file site",
 )
 
 
