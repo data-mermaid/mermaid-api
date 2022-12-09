9dd1aa69486b6b600c3204d6c167711ac1e8c00c
Dustin Sampson <dustin@sparkgeo.com>
2022-12-09 13:52:12 -0800

    wip

---
+++ src/api/signals/attributes.py
@@ -0,0 +1,35 @@
+from django.db.models.signals import post_delete, post_save
+from django.dispatch import receiver
+
+from ..models import (
+    SUPERUSER_APPROVED,
+    BenthicAttribute,
+    FishFamily,
+    FishGenus,
+    FishGrouping,
+    FishSpecies,
+    Region,
+)
+from ..reports import attributes_report
+from ..utils.q import submit_job
+
+
+@receiver(post_delete, sender=BenthicAttribute)
+@receiver(post_save, sender=BenthicAttribute)
+@receiver(post_delete, sender=FishFamily)
+@receiver(post_save, sender=FishFamily)
+@receiver(post_delete, sender=FishGenus)
+@receiver(post_save, sender=FishGenus)
+@receiver(post_delete, sender=FishGrouping)
+@receiver(post_save, sender=FishGrouping)
+@receiver(post_delete, sender=FishSpecies)
+@receiver(post_save, sender=FishSpecies)
+def generate_attribute_report(sender, instance, **kwargs):
+    if instance.status == SUPERUSER_APPROVED:
+        attributes_report.write_attribute_reference()
+        # submit_job(
+        #     delay=10,
+
+        # )
+
+
