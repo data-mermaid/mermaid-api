632e0ef22e13a6bcc6579c21a4af64e58b52768d
Kim Fisher <kfisher@wcs.org>
2023-02-01 11:13:26 -0500

    temporarily suspending update_project_summaries

--- src/api/utils/summaries.py
+++ src/api/utils/summaries.py
@@ -4,7 +4,8 @@ from ..models import Project, SummarySampleEventModel, SummarySampleEventSQLMode
 
 
 def update_project_summaries(project_id, skip_test_project=True, *args, **kwargs):
-    update_project_summary_sample_event(project_id, skip_test_project)
+    pass  # stop the fires burning
+    # update_project_summary_sample_event(project_id, skip_test_project)
 
 
 def update_project_summary_sample_event(project_id, skip_test_project=True):
