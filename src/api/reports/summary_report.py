398ec2903ef79e1063070beaca83578cb43cacca
Dustin Sampson <dustin@sparkgeo.com>
2023-02-14 11:14:56 -0800

    Refactor summary reports to use pyexcelerate

--- src/api/reports/summary_report.py
+++ src/api/reports/summary_report.py
@@ -1,64 +1,40 @@
 import csv
-from collections import defaultdict
 
-from openpyxl import Workbook
-from openpyxl.styles import DEFAULT_FONT, Font
-from openpyxl.utils.cell import coordinate_to_tuple, get_column_letter
+from pyexcelerate import Font, Style, Workbook
 
 from ..mocks import MockRequest
 from ..resources.sampleunitmethods.beltfishmethod import (
     BeltFishProjectMethodObsView,
-    BeltFishProjectMethodSUView,
     BeltFishProjectMethodSEView,
-)
-from ..resources.sampleunitmethods.benthicpitmethod import (
-    BenthicPITProjectMethodObsView,
-    BenthicPITProjectMethodSUView,
-    BenthicPITProjectMethodSEView,
+    BeltFishProjectMethodSUView,
 )
 from ..resources.sampleunitmethods.benthiclitmethod import (
     BenthicLITProjectMethodObsView,
-    BenthicLITProjectMethodSUView,
     BenthicLITProjectMethodSEView,
+    BenthicLITProjectMethodSUView,
 )
-from ..resources.sampleunitmethods.habitatcomplexitymethod import (
-    HabitatComplexityProjectMethodObsView,
-    HabitatComplexityProjectMethodSUView,
-    HabitatComplexityProjectMethodSEView,
+from ..resources.sampleunitmethods.benthicphotoquadrattransectmethod import (
+    BenthicPQTProjectMethodObsView,
+    BenthicPQTProjectMethodSEView,
+    BenthicPQTProjectMethodSUView,
+)
+from ..resources.sampleunitmethods.benthicpitmethod import (
+    BenthicPITProjectMethodObsView,
+    BenthicPITProjectMethodSEView,
+    BenthicPITProjectMethodSUView,
 )
 from ..resources.sampleunitmethods.bleachingquadratcollectionmethod import (
     BleachingQCProjectMethodObsColoniesBleachedView,
     BleachingQCProjectMethodObsQuadratBenthicPercentView,
-    BleachingQCProjectMethodSUView,
     BleachingQCProjectMethodSEView,
+    BleachingQCProjectMethodSUView,
 )
-from ..resources.sampleunitmethods.benthicphotoquadrattransectmethod import (
-    BenthicPQTProjectMethodObsView,
-    BenthicPQTProjectMethodSUView,
-    BenthicPQTProjectMethodSEView,
+from ..resources.sampleunitmethods.habitatcomplexitymethod import (
+    HabitatComplexityProjectMethodObsView,
+    HabitatComplexityProjectMethodSEView,
+    HabitatComplexityProjectMethodSUView,
 )
-
-DEFAULT_FONT_NAME = "Arial"
-HEADER_FONT = Font(name=DEFAULT_FONT_NAME, bold=True)
-
-
-def insert_range_and_resize(ws, rng, data, bold_header=True):
-    _data = data or []
-    row, col = coordinate_to_tuple(rng)
-    dims = defaultdict(int)
-    for n, row_val in enumerate(_data):
-        for m, val in enumerate(row_val):
-            ws.cell(row=row + n, column=col + m, value=val)
-
-            # Bold header row
-            if n == 0 and bold_header:
-                ws.cell(row=row + n, column=col + m).font = HEADER_FONT
-
-            width = len(str(val)) + 2
-            dims[col + m] = max(dims[col + m], width)
-
-    for col, value in dims.items():
-        ws.column_dimensions[get_column_letter(col)].width = value
+from ..utils.timer import timing
 
 
 def get_viewset_csv_content(view_cls, project_pk, request):
@@ -72,169 +48,120 @@ def get_viewset_csv_content(view_cls, project_pk, request):
     )
 
 
-def create_workbook_template(sheet_names):
-    wb = Workbook()
-    DEFAULT_FONT.name = DEFAULT_FONT_NAME
+def write_data(wb, sheet_name, data):
+    ws = wb.new_sheet(sheet_name, data=data)
+    ws.set_row_style(1, Style(font=Font(bold=True)))
 
-    wb.remove(wb.active)
-    for sheet_name in sheet_names:
-        wb.create_sheet(title=sheet_name)
+
+@timing
+def _create_report(request, project_pk, views, sheet_names):
+    wb = Workbook()
+    for view, sheet_name in zip(views, sheet_names):
+        content = get_viewset_csv_content(view, project_pk, request)
+        write_data(wb, sheet_name, content)
 
     return wb
 
 
 def create_belt_fish_report(request, project_pk):
-    worksheet_names = [
-        "Belt Fish Obs",
-        "Belt Fish SU",
-        "Belt Fish SE",
-    ]
-    wb = create_workbook_template(worksheet_names)
-
-    content = get_viewset_csv_content(BeltFishProjectMethodObsView, project_pk, request)
-    insert_range_and_resize(wb[worksheet_names[0]], "A1", content)
-
-    content = get_viewset_csv_content(BeltFishProjectMethodSUView, project_pk, request)
-    insert_range_and_resize(wb[worksheet_names[1]], "A1", content)
-
-    content = get_viewset_csv_content(BeltFishProjectMethodSEView, project_pk, request)
-    insert_range_and_resize(wb[worksheet_names[2]], "A1", content)
-
-    return wb
+    return _create_report(
+        request,
+        project_pk,
+        views=[
+            BeltFishProjectMethodObsView,
+            BeltFishProjectMethodSUView,
+            BeltFishProjectMethodSEView,
+        ],
+        sheet_names=[
+            "Belt Fish Obs",
+            "Belt Fish SU",
+            "Belt Fish SE",
+        ],
+    )
 
 
 def create_benthic_pit_report(request, project_pk):
-    worksheet_names = [
-        "Benthic PIT Obs",
-        "Benthic PIT SU",
-        "Benthic PIT SE",
-    ]
-    wb = create_workbook_template(worksheet_names)
-
-    content = get_viewset_csv_content(
-        BenthicPITProjectMethodObsView, project_pk, request
+    return _create_report(
+        request,
+        project_pk,
+        views=[
+            BenthicPITProjectMethodObsView,
+            BenthicPITProjectMethodSUView,
+            BenthicPITProjectMethodSEView,
+        ],
+        sheet_names=[
+            "Benthic PIT Obs",
+            "Benthic PIT SU",
+            "Benthic PIT SE",
+        ],
     )
-    insert_range_and_resize(wb[worksheet_names[0]], "A1", content)
-
-    content = get_viewset_csv_content(
-        BenthicPITProjectMethodSUView, project_pk, request
-    )
-    insert_range_and_resize(wb[worksheet_names[1]], "A1", content)
-
-    content = get_viewset_csv_content(
-        BenthicPITProjectMethodSEView, project_pk, request
-    )
-    insert_range_and_resize(wb[worksheet_names[2]], "A1", content)
-
-    return wb
 
 
 def create_benthic_lit_report(request, project_pk):
-    worksheet_names = [
-        "Benthic LIT Obs",
-        "Benthic LIT SU",
-        "Benthic LIT SE",
-    ]
-    wb = create_workbook_template(worksheet_names)
-
-    content = get_viewset_csv_content(
-        BenthicLITProjectMethodObsView, project_pk, request
+    return _create_report(
+        request,
+        project_pk,
+        views=[
+            BenthicLITProjectMethodObsView,
+            BenthicLITProjectMethodSUView,
+            BenthicLITProjectMethodSEView,
+        ],
+        sheet_names=[
+            "Benthic LIT Obs",
+            "Benthic LIT SU",
+            "Benthic LIT SE",
+        ],
     )
-    insert_range_and_resize(wb[worksheet_names[0]], "A1", content)
-
-    content = get_viewset_csv_content(
-        BenthicLITProjectMethodSUView, project_pk, request
-    )
-    insert_range_and_resize(wb[worksheet_names[1]], "A1", content)
-
-    content = get_viewset_csv_content(
-        BenthicLITProjectMethodSEView, project_pk, request
-    )
-    insert_range_and_resize(wb[worksheet_names[2]], "A1", content)
-
-    return wb
 
 
 def create_bleaching_qc_report(request, project_pk):
-    worksheet_names = [
-        "BQT Colonies Bleached Obs",
-        "BQT Quad Benthic Percent Obs",
-        "Bleaching QT SU",
-        "Bleaching QT SE",
-    ]
-
-    wb = create_workbook_template(worksheet_names)
-
-    content = get_viewset_csv_content(
-        BleachingQCProjectMethodObsColoniesBleachedView, project_pk, request
+    return _create_report(
+        request,
+        project_pk,
+        views=[
+            BleachingQCProjectMethodObsColoniesBleachedView,
+            BleachingQCProjectMethodObsQuadratBenthicPercentView,
+            BleachingQCProjectMethodSUView,
+            BleachingQCProjectMethodSEView,
+        ],
+        sheet_names=[
+            "BQT Colonies Bleached Obs",
+            "BQT Quad Benthic Percent Obs",
+            "Bleaching QT SU",
+            "Bleaching QT SE",
+        ],
     )
-    insert_range_and_resize(wb[worksheet_names[0]], "A1", content)
-
-    content = get_viewset_csv_content(
-        BleachingQCProjectMethodObsQuadratBenthicPercentView, project_pk, request
-    )
-    insert_range_and_resize(wb[worksheet_names[1]], "A1", content)
-
-    content = get_viewset_csv_content(
-        BleachingQCProjectMethodSUView, project_pk, request
-    )
-    insert_range_and_resize(wb[worksheet_names[2]], "A1", content)
-
-    content = get_viewset_csv_content(
-        BleachingQCProjectMethodSEView, project_pk, request
-    )
-    insert_range_and_resize(wb[worksheet_names[3]], "A1", content)
-
-    return wb
 
 
 def create_benthic_pqt_report(request, project_pk):
-    worksheet_names = [
-        "Benthic PQT Obs",
-        "Benthic PQT SU",
-        "Benthic PQT SE",
-    ]
-    wb = create_workbook_template(worksheet_names)
-
-    content = get_viewset_csv_content(
-        BenthicPQTProjectMethodObsView, project_pk, request
+    return _create_report(
+        request,
+        project_pk,
+        views=[
+            BenthicPQTProjectMethodObsView,
+            BenthicPQTProjectMethodSUView,
+            BenthicPQTProjectMethodSEView,
+        ],
+        sheet_names=[
+            "Benthic PQT Obs",
+            "Benthic PQT SU",
+            "Benthic PQT SE",
+        ],
     )
-    insert_range_and_resize(wb[worksheet_names[0]], "A1", content)
-
-    content = get_viewset_csv_content(
-        BenthicPQTProjectMethodSUView, project_pk, request
-    )
-    insert_range_and_resize(wb[worksheet_names[1]], "A1", content)
-
-    content = get_viewset_csv_content(
-        BenthicPQTProjectMethodSEView, project_pk, request
-    )
-    insert_range_and_resize(wb[worksheet_names[2]], "A1", content)
-
-    return wb
 
 
 def create_habitat_complexity_report(request, project_pk):
-    worksheet_names = [
-        "Habitat Complexity Obs",
-        "Habitat Complexity SU",
-        "Habitat Complexity SE",
-    ]
-    wb = create_workbook_template(worksheet_names)
-
-    content = get_viewset_csv_content(
-        HabitatComplexityProjectMethodObsView, project_pk, request
+    return _create_report(
+        request,
+        project_pk,
+        views=[
+            HabitatComplexityProjectMethodObsView,
+            HabitatComplexityProjectMethodSUView,
+            HabitatComplexityProjectMethodSEView,
+        ],
+        sheet_names=[
+            "Habitat Complexity Obs",
+            "Habitat Complexity SU",
+            "Habitat Complexity SE",
+        ],
     )
-    insert_range_and_resize(wb[worksheet_names[0]], "A1", content)
-
-    content = get_viewset_csv_content(
-        HabitatComplexityProjectMethodSUView, project_pk, request
-    )
-    insert_range_and_resize(wb[worksheet_names[1]], "A1", content)
-
-    content = get_viewset_csv_content(
-        HabitatComplexityProjectMethodSEView, project_pk, request
-    )
-    insert_range_and_resize(wb[worksheet_names[2]], "A1", content)
-
-    return wb
