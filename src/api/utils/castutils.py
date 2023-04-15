d8bc09328165aff339d6531a48c4460ff63d76b7
Dustin Sampson <dustin@sparkgeo.com>
2023-04-15 09:40:51 -0700

    Cast excel data before writing to worksheet

--- src/api/utils/castutils.py
+++ src/api/utils/castutils.py
@@ -9,3 +9,20 @@ def to_decimal(val, max_digits, precision):
         ctx.prec = max_digits
         places = Decimal(10) ** (precision * -1)
         return Decimal(val).quantize(places, ROUND_HALF_DOWN)
+
+
+def to_number(string):
+    try:
+        num = float(string)
+        return int(num) if num.is_integer() else num
+    except ValueError:
+        return None
+
+
+def cast_str_value(string):
+    if not isinstance(string, str):
+        return string
+
+    val = to_number(string)
+    return val if val is not None else string
+    
