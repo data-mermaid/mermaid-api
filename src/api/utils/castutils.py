648b06963c91b5f4771e51de0ff64bed3db52449
Dustin Sampson <dustin@sparkgeo.com>
2023-04-17 07:06:02 -0700

    Change number casting

--- src/api/utils/castutils.py
+++ src/api/utils/castutils.py
@@ -2,20 +2,17 @@ from decimal import ROUND_HALF_DOWN, Decimal, localcontext
 
 
 def to_number(string, max_digits=None, precision=None):
-    if str(string) == "" or string is None:
-        return None
-
-    if max_digits is None or precision is None:
-        try:
+    try:
+        if max_digits is None or precision is None:
             num = float(string)
             return int(num) if num.is_integer() else num
-        except (TypeError, ValueError) as e:
-            return None
-
-    with localcontext() as ctx:
-        ctx.prec = max_digits
-        places = Decimal(10) ** (precision * -1)
-        return Decimal(string).quantize(places, ROUND_HALF_DOWN)
+        else:
+            with localcontext() as ctx:
+                ctx.prec = max_digits
+                places = Decimal(10) ** (precision * -1)
+                return Decimal(string).quantize(places, ROUND_HALF_DOWN)
+    except (TypeError, ValueError) as e:
+        return None
 
 
 def cast_str_value(string):
