eaa034e81eb25a6e781f5ee199731bf96db5005a
Dustin Sampson <dustin@sparkgeo.com>
2023-09-26 15:02:19 -0700

    break up large covar lookup function

--- src/api/reports/summary_report.py
+++ src/api/reports/summary_report.py
@@ -42,7 +42,22 @@ aca_benthic_key, aca_benthic_field = Covariate.SUPPORTED_COVARIATES[0]
 aca_geomorphic_key, aca_geomorphic_field = Covariate.SUPPORTED_COVARIATES[1]
 
 
-def _covariate_lookup(site_id_index, content):
+def _update_covariate_lookup(covar_lookup, site_id, covariates):
+    covar_lookup[site_id] = ["", ""]
+    for covariate in covariates:
+        values = covariate.value
+        if values is None:
+            continue
+        covar_name = covariate.name
+        if covar_name == aca_benthic_key:
+            values = sorted(values, key=lambda x: (x["area"]), reverse=True)
+            covar_lookup[site_id][0] = values[0]["name"] if values else ""
+        elif covar_name == aca_geomorphic_key:
+            values = sorted(values, key=lambda x: (x["area"]), reverse=True)
+            covar_lookup[site_id][1] = values[0]["name"] if values else ""
+
+
+def _covariate_aca_lookup(site_id_index, content):
     if isinstance(content, list) is False or len(content) < 2:
         return None
 
@@ -57,27 +72,19 @@ def _covariate_lookup(site_id_index, content):
 
         site_id = str(s.id)
         covar_lookup[site_id] = ["", ""]
-        for covariate in covariates:
-            values = covariate.value
-            if values is None:
-                continue
-            covar_name = covariate.name
-            if covar_name == aca_benthic_key:
-                values = sorted(values, key=lambda x: (x["area"]), reverse=True)
-                covar_lookup[site_id][0] = values[0]["name"] if values else ""
-            elif covar_name == aca_geomorphic_key:
-                values = sorted(values, key=lambda x: (x["area"]), reverse=True)
-                covar_lookup[site_id][1] = values[0]["name"] if values else ""
+        _update_covariate_lookup(covar_lookup, site_id, covariates)
 
     return covar_lookup
 
 
-def _append_covariates(site_id_index, content, view_serializer):
+def _append_aca_covariates(site_id_index, content, view_serializer):
+    """ Append Alan Coral Atlas covariates """
+
     num_additional_fields = len(view_serializer.additional_fields)
     column_names = [f.display for f in view_serializer.fields]
     column_names.extend([aca_benthic_field, aca_geomorphic_field])
 
-    covariate_lookup = _covariate_lookup(site_id_index, content)
+    covariate_lookup = _covariate_aca_lookup(site_id_index, content)
     data = [column_names]
     for row in content[1:]:
         site_id = row[site_id_index]
@@ -106,7 +113,7 @@ def get_viewset_csv_content(view_cls, project_pk, request):
     if site_id_index is None:
         return content
     
-    return _append_covariates(
+    return _append_aca_covariates(
         site_id_index,
         content,
         view_cls.serializer_class_csv
