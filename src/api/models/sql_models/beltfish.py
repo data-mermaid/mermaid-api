0f315a67afc6b89fda7d22128395cc1bc9c76b3e
Kim Fisher <kfisher@wcs.org>
2023-11-29 12:55:48 -0500

    include beltfish_su_family_zeroes in CTE

--- src/api/models/sql_models/beltfish.py
+++ src/api/models/sql_models/beltfish.py
@@ -214,11 +214,6 @@ class BeltFishSUSQLModel(BaseSUSQLModel):
 			FROM beltfish_obs
 			GROUP BY pseudosu_id, trophic_group
 		),
-		beltfish_su_family_all AS MATERIALIZED (SELECT
-		    pseudosu_id, fish_family.name AS fish_family
-		    FROM fish_family CROSS JOIN beltfish_obs
-		    GROUP BY pseudosu_id, fish_family.name
-		),
 		beltfish_su_family AS MATERIALIZED (SELECT
             pseudosu_id,
             fish_family,
@@ -226,6 +221,21 @@ class BeltFishSUSQLModel(BaseSUSQLModel):
             FROM beltfish_obs
             GROUP BY pseudosu_id, fish_family
         ),
+		beltfish_su_family_zeroes AS MATERIALIZED (SELECT 
+			beltfish_su_family_all.pseudosu_id,
+			beltfish_su_family_all.fish_family,
+			COALESCE(beltfish_su_family.biomass_kgha, 0) AS biomass_kgha
+			FROM (
+				SELECT
+				pseudosu_id, fish_family.name AS fish_family
+				FROM fish_family CROSS JOIN beltfish_obs
+				GROUP BY pseudosu_id, fish_family.name
+			) beltfish_su_family_all
+			LEFT JOIN beltfish_su_family ON (
+				beltfish_su_family_all.pseudosu_id = beltfish_su_family.pseudosu_id
+				AND beltfish_su_family_all.fish_family = beltfish_su_family.fish_family
+			)
+		),
         
 		beltfish_tg AS MATERIALIZED (SELECT
             beltfish_su_tg.pseudosu_id,
@@ -270,17 +280,7 @@ class BeltFishSUSQLModel(BaseSUSQLModel):
             ) AS biomass_kgha_fish_family_zeroes
     
             FROM beltfish_su_family
-            INNER JOIN (
-                SELECT 
-                beltfish_su_family_all.pseudosu_id,
-                beltfish_su_family_all.fish_family,
-                COALESCE(beltfish_su_family.biomass_kgha, 0) AS biomass_kgha
-                FROM beltfish_su_family_all
-                LEFT JOIN beltfish_su_family ON(
-                    beltfish_su_family_all.pseudosu_id = beltfish_su_family.pseudosu_id
-                    AND beltfish_su_family_all.fish_family = beltfish_su_family.fish_family
-                )
-            ) beltfish_su_family_zeroes
+            INNER JOIN beltfish_su_family_zeroes
             ON(beltfish_su_family.pseudosu_id = beltfish_su_family_zeroes.pseudosu_id)
             GROUP BY beltfish_su_family.pseudosu_id
         ),
