5d21318c33d6ec3906c24d62fc1494b5614a20f8
Dustin Sampson <dustin@sparkgeo.com>
2024-03-26 16:17:05 -0700

    Break up project sample summary function
    - broke up main view function to make more readable
    - added castutil to cast string iso format date/datetime strings to datetime instance
    - includes query parameter to filter by last_created_on

--- src/api/resources/sample_event_summaries.py
+++ src/api/resources/sample_event_summaries.py
@@ -6,8 +6,8 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.utils.urls import replace_query_param
 
-from api.utils.timer import Timer
 from ..models import Project, SummarySampleEventModel
+from ..utils.castutils import iso8601_to_datetime
 from .summary_sample_event import SummarySampleEventSerializer
 
 
@@ -16,12 +16,63 @@ def _build_url(request, page_num):
     return replace_query_param(url, "page", page_num)
 
 
+def _get_summaries(profile, page_num, limit, last_created_on=None):
+    qs = SummarySampleEventModel.objects.filter(~Q(project_status=Project.TEST))
+    if last_created_on:
+        count_qs = qs.filter(created_on__gt=last_created_on)
+    else:
+        count_qs = qs
+
+    unique_project_ids = count_qs.values_list("project_id", flat=True).distinct()
+    count = len(unique_project_ids)
+
+    start_index = (page_num - 1) * limit
+    end_index = page_num * limit
+    return count, qs.filter(project_id__in=unique_project_ids[start_index:end_index]) \
+        .privatize(profile=profile) \
+        .order_by("project_id")
+    
+
+def _group_by_project_id(qs):
+    project_id = None
+    project_records = []
+    for summary in qs:
+        if project_id != summary.project_id:
+            project_id = summary.project_id
+            if project_records:
+                yield {
+                    "project_id": project_id,
+                    "records": SummarySampleEventSerializer(
+                        project_records, many=True
+                    ).data,
+                }
+            project_records = []
+        project_records.append(summary)
+
+    if project_records:
+        yield {
+            "project_id": project_id,
+            "records": SummarySampleEventSerializer(project_records, many=True).data,
+        }
+
+
 @api_view(http_method_names=["GET"])
 @authentication_classes([])
 @permission_classes((AllowAny,))
 def vw_project_sample_event_summaries(request):
     """
     Returns a paginated list of all sample events by project.  Test projects are excluded.
+
+    Example Response:
+    {
+        "count": 79,
+        "next": null,
+        "previous": "http://localhost:8080/v1/project_sample_events/?limit=25&page=3",
+        "results": [
+            {project_id: 1, records: [{...}, {...}, ...]},
+            {project_id: 2, records: [{...}, {...}, ...]},
+            ...
+        ]
     """
     try:
         limit = int(request.query_params.get("limit", 100))
@@ -33,63 +84,33 @@ def vw_project_sample_event_summaries(request):
     except ValueError:
         return Response("Invalid page", status=400)
 
+    try:
+        last_created_on = iso8601_to_datetime(request.query_params.get("last_created_on"))
+    except ValueError:
+        last_created_on = None
+
     is_secure = request.user.is_authenticated
     if is_secure:
         profile = request.user.profile
     else:
         profile = None
-
-    qs = SummarySampleEventModel.objects.filter(~Q(project_status=Project.TEST))
-    unique_project_ids = qs.values_list("project_id", flat=True).distinct()
-    count = len(unique_project_ids)
-
-    start_index = (page_num - 1) * limit
-    end_index = page_num * limit
-    summaries = (
-        qs.filter(project_id__in=unique_project_ids[start_index:end_index])
-        .privatize(profile=profile)
-        .order_by("project_id")
-    )
-
-    project_id = None
-    results = []
-    project_records = []
-    for summary in summaries:
-        if project_id != summary.project_id:
-            project_id = summary.project_id
-            if project_records:
-                results.append(
-                    {
-                        "project_id": project_id,
-                        "records": SummarySampleEventSerializer(
-                            project_records, many=True
-                        ).data,
-                    }
-                )
-            project_records = []
-        project_records.append(summary)
-
-    if project_records:
-        results.append(
-            {
-                "project_id": project_id,
-                "records": SummarySampleEventSerializer(project_records, many=True).data,
-            }
-        )
+    
+    count, summaries = _get_summaries(profile, page_num, limit, last_created_on)
+    results = _group_by_project_id(summaries)
 
     total_pages = math.ceil(count / limit)
-    if total_pages > page_num:
+    if total_pages > 0 and total_pages > page_num:
         next_page_num = page_num + 1
         next_url = _build_url(request, next_page_num)
     else:
         next_url = None
 
-    if page_num > 1:
+    if total_pages > 0 and page_num > 1:
         previous_page_num = page_num - 1
         previous_url = _build_url(request, previous_page_num)
     else:
         previous_url = None
 
     return Response(
-        {"count": count, "next": next_url, "previous": previous_url, "results": results}
+        {"count": count, "next": next_url, "previous": previous_url, "results": list(results)}
     )
