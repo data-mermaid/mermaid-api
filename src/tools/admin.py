8e6499287633352b701870a17572b41bfefced8e
Dustin Sampson <dustin@sparkgeo.com>
2024-11-28 07:58:47 -0800

    user metrics model and admin

---
+++ src/tools/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from api.admin import BaseAdmin
+from .models import UserMetrics
+
+
+@admin.register(UserMetrics)
+class ProjectAdmin(BaseAdmin):
+    list_per_page = 50
+    list_display = (
+        "date",
+        "project_name",
+        "name",
+        "num_submitted",
+        "num_summary_views",
+        "num_project_calls",
+        "num_image_uploads",
+    )
+    ordering = ("-date", "project_name", "first_name", "last_name")
+    list_filter = ("date", "project_name")
+    search_fields = ("project_name", "first_name", "last_name")
+    readonly_fields = [field.name for field in UserMetrics._meta.fields]
+
+    def name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
--- src/tools/models.py
+++ src/tools/models.py
@@ -10,3 +10,30 @@ class LogEvent(models.Model):
 
     def __str__(self):
         return f"{self.timestamp} {self.event}"
+
+
+class UserMetrics(models.Model):
+    date = models.DateField()
+    profile = models.ForeignKey(
+        "api.Profile",
+        related_name="user_metrics",
+        on_delete=models.DO_NOTHING,
+        null=True,
+        blank=True,
+    )
+    role = models.CharField(max_length=10, null=True, blank=True)
+    project_tags = models.TextField()
+    countries = models.TextField()
+    project_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=50, null=True, blank=True)
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+    email = models.CharField(max_length=50, null=True, blank=True)
+    project_status = models.CharField(max_length=10)
+    num_submitted = models.IntegerField(default=0)
+    num_summary_views = models.IntegerField(default=0)
+    num_project_calls = models.IntegerField(default=0)
+    num_image_uploads = models.IntegerField(default=0)
+    profiles = models.JSONField(null=True, blank=True)
+
+    class Meta:
+        db_table = "user_metrics"
