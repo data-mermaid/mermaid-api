9b90587e955b3620b9ee90559e7d1c0d8e3c6b9e
Kim Fisher <kfisher@wcs.org>
2025-05-19 17:46:15 -0400

    prod IC handling and annotations parquet export

--- src/api/utils/s3.py
+++ src/api/utils/s3.py
@@ -8,22 +8,39 @@ from django.conf import settings
 logger = logging.getLogger(__name__)
 
 
-def get_client():
+def get_client(
+    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+):
     session = boto3.session.Session(
-        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
         region_name=settings.AWS_REGION,
     )
     return session.client("s3")
 
 
-def get_object(bucket, key):
-    client = get_client()
+def get_object(
+    bucket,
+    key,
+    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+):
+    client = get_client(
+        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key
+    )
     return client.get_object(Bucket=bucket, Key=key)
 
 
-def delete_file(bucket, blob_name):
-    client = get_client()
+def delete_file(
+    bucket,
+    blob_name,
+    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+):
+    client = get_client(
+        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key
+    )
     try:
         client.delete_object(Bucket=bucket, Key=blob_name)
         return True
@@ -36,8 +53,18 @@ def delete_file(bucket, blob_name):
         return False
 
 
-def upload_file(bucket, local_file_path, blob_name, content_type=None, content_encoding=None):
-    client = get_client()
+def upload_file(
+    bucket,
+    local_file_path,
+    blob_name,
+    content_type=None,
+    content_encoding=None,
+    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+):
+    client = get_client(
+        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key
+    )
 
     extra_args = {}
     if content_type:
@@ -48,13 +75,28 @@ def upload_file(bucket, local_file_path, blob_name, content_type=None, content_e
     client.upload_file(local_file_path, bucket, blob_name, ExtraArgs=extra_args)
 
 
-def download_file(bucket, blob_name, local_file_path):
-    client = get_client()
+def download_file(
+    bucket,
+    blob_name,
+    local_file_path,
+    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+):
+    client = get_client(
+        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key
+    )
     client.download_file(bucket, blob_name, local_file_path)
 
 
-def file_exists(bucket, blob_name):
-    client = get_client()
+def file_exists(
+    bucket,
+    blob_name,
+    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+):
+    client = get_client(
+        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key
+    )
     try:
         client.head_object(Bucket=bucket, Key=blob_name)
         return True
@@ -64,8 +106,16 @@ def file_exists(bucket, blob_name):
         raise
 
 
-def download_directory(bucket, s3_directory, local_directory):
-    client = get_client()
+def download_directory(
+    bucket,
+    s3_directory,
+    local_directory,
+    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+):
+    client = get_client(
+        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key
+    )
     paginator = client.get_paginator("list_objects_v2")
     pages = paginator.paginate(Bucket=bucket, Prefix=s3_directory)
 
@@ -79,8 +129,16 @@ def download_directory(bucket, s3_directory, local_directory):
             client.download_file(bucket, s3_key, local_path)
 
 
-def get_presigned_url(bucket, key, expiration=604_800):
-    client = get_client()
+def get_presigned_url(
+    bucket,
+    key,
+    expiration=604_800,
+    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+):
+    client = get_client(
+        aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key
+    )
     return client.generate_presigned_url(
         "get_object",
         Params={"Bucket": bucket, "Key": key},
