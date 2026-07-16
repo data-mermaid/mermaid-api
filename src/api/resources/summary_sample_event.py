8183dc60117872d73f92ab27b2e4e4b3493dfea3
Kim Fisher <kfisher@wcs.org>
2026-07-16 18:04:32 -0400

    add depth_avg/depth_sd to summary SE views

--- src/api/resources/summary_sample_event.py
+++ src/api/resources/summary_sample_event.py
@@ -63,6 +63,8 @@ class SummarySampleEventCSVSerializer(ReportSerializer):
         ReportField("reef_exposure", "Exposure"),
         ReportField("reef_type", "Reef type"),
         ReportField("reef_zone", "Reef zone"),
+        ReportField("depth_avg", "Depth average"),
+        ReportField("depth_sd", "Depth standard deviation"),
         ReportField("sample_date", "Year", to_year, "sample_date_year"),
         ReportField("sample_date", "Month", to_month, "sample_date_month"),
         ReportField("sample_date", "Day", to_day, "sample_date_day"),
