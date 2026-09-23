6fd06327042c0661bab02976d697ae4403ebe922
Dustin Sampson <dustin@sparkgeo.com>
2026-09-23 09:44:18 -0700

    slight ram bump on dev api server

--- iac/settings/dev.py
+++ iac/settings/dev.py
@@ -16,7 +16,8 @@ DEV_SETTINGS = ProjectSettings(
     api=DjangoSettings(
         # API
         container_cpu=1024,
-        container_memory=2048,
+        container_memory=2560,
+
         container_count=1,
         # SQS
         sqs_cpu=1024,
